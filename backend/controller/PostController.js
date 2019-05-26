925a17da5413c08a77a0442f6323e665e3ac5e08
ben <huang831206@gmail.com>
2019-05-26 22:05:13 +0800

    add express routing and post controller

---
+++ backend/controller/PostController.js
@@ -0,0 +1,14 @@
+var post = {}
+
+post.trending = function (req, res) {
+    // return trending posts
+}
+
+post.vote = function (req, res) {
+    // vote for a post
+}
+
+post.create = function (req, res) {
+    // create a new post
+}
+module.exports = home;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,19 @@
 
 var router = function(app) {
+    
+    // the post controller
+    var post = require('../controller/PostController.js')
 
     app.get('/', function(req, res) {
         res.end('home')
     })
+
+    // get trending post list
+    app.get('/post/trending', post.trending)
+    // vote for a post
+    app.post('/post/vote', post.vote)
+    // create a new post
+    app.post('/post/create', post.create)
 
 }
 
