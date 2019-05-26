409ca0f8788a5b8257fc562cb4b44219dd1d71c4
ben <huang831206@gmail.com>
2019-05-27 00:44:26 +0800

    add storage test

---
+++ test/backend/storage.test.js
@@ -0,0 +1,58 @@
+import test from 'ava';
+
+/*** test the MemoryStorage.js module itself ***/
+
+test('test memory storage: insert', t => {
+    const MS = require('../../backend/storage/MemoryStorage.js')
+    const ms = new MS()
+    let obj = {a: 1, b: '2'}
+    let id = ms.insert(obj)
+
+    t.deepEqual(ms.data, new Map([[id, obj]]))
+})
+
+test('test memory storage: select', t => {
+    const MS = require('../../backend/storage/MemoryStorage.js')
+    const ms = new MS()
+    let obj = {a: 1, b: '2'}
+    let id = ms.insert(obj)
+
+    t.deepEqual(ms.select(id), obj)
+})
+
+test('test memory storage: select all', t => {
+    const MS = require('../../backend/storage/MemoryStorage.js')
+    const ms = new MS()
+    let obj1 = {a: 1, b: '2'}
+    let obj2 = {a: 345, b: '678'}
+    ms.insert(obj1)
+    ms.insert(obj2)
+
+    t.deepEqual(ms.select('*'), [obj1, obj2])
+})
+
+test('test memory storage: selectIn', t => {
+    const MS = require('../../backend/storage/MemoryStorage.js')
+    const ms = new MS()
+    let obj1 = {a: 1, b: '2'}
+    let obj2 = {a: 345, b: '678'}
+    let id1 = ms.insert(obj1)
+    let id2 = ms.insert(obj2)
+
+    t.deepEqual(ms.selectIn([id1, id2]), [obj1, obj2])
+})
+
+/******/
+
+
+/*** test if DB.js can load providers successfully ***/
+
+test('test db loader', t => {
+    let DB = require('../../backend/DB.js')
+    let providerInstance = new DB().use('memory')
+    let MemoryStorage = require('../../backend/storage/MemoryStorage.js')
+
+    t.true(providerInstance instanceof MemoryStorage)
+})
+
+/******/
