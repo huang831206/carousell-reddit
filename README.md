62bcb6d9bc554c7b0338cd5e4c28fb370380d781
ben <huang831206@gmail.com>
2019-05-26 22:40:10 +0800

    add basic in-memory CRUD

---
+++ README.md
@@ -0,0 +1,10 @@
+# Carousell reddit clone
+
+## frontend
+
+
+## backend
+### storage
+The implementation of storages.
+Try to reduce the trouble to swap storage options. So `BaseStorage.js` is more like a interface or template, regulating what should be done to provide storage functionality.
+In this case, `MemoryStorage.js` provides the option to save data in memory.
--- backend/storage/MemoryStorage.js
+++ backend/storage/MemoryStorage.js
@@ -9,32 +9,65 @@ class MemoryStorage extends BaseStorage{
 
     // add a new record
     insert(item){
+        if ((!!item) && (item.constructor === Object)) {
+            let uuid = require('uuid/v4')
+            item.id = uuid()
+            this.data.set(item.id, item)
+            return item.id
+        }
+        return false
+    }
 
+    // add mutiple records
+    bulkInsert(items){
+        if (Array.isArray(items)) {
+            let uuid = require('uuid/v4')
+            for (const item of items) {
+                item.id = uuid()
+                this.data.set(item.id, item)
+            }
+            return true
+        }
+        return false
     }
     
     // select record by given id
-    select(id){
-
+    // raw: if ture, return the exact object in data map. by default this function returns a clone
+    select(id, raw = false){
+        if (id === '*'){
+            return raw ? [...this.data.values()] : JSON.parse(JSON.stringify([...this.data.values()]))
+        }else if(id){
+            return raw ? this.data.get(id) : JSON.parse(JSON.stringify(this.data.get(id)))
+        }
+        return false
     }
 
     // select records where id in ids
-    selectIn(ids){
-
+    // raw: if ture, return the exact object in data map. by default this function returns clones
+    selectIn(ids, raw = false){
+        let results = []
+        this.data.forEach( (value, id) => {
+            if (ids.includes(id)) {
+                results.push(raw ? value : JSON.parse(JSON.stringify(value)))
+            }
+        })
+        return results
     }
 
     // does the post with given id exist
     has(id){
-
+        return this.data.has(id)
     }
 
     // there's only one update action(update votes) in this app
-    update(id){
-
+    // type: 'upVotes' or 'downVotes'
+    update(id, type){
+        this.data.get(id)[type]++
     }
 
     // delete a certain post
     delete(id){
-
+        return this.data.delete(id)
     }
 
 }
