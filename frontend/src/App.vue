f70230c2b862f7ff7d9b4f1adc147d45e72c69cd
ben <huang831206@gmail.com>
2019-05-26 21:45:52 +0800

    add Post.vue component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="header">
+            <h1>Top 20 Popular Posts</h1>
+        </div>
+        <div class="posts-container">
+            <!-- i for indicating it's rank -->
+            <Post v-for="(post,index) in posts" :post="post" :key="post.id" :i="index"></Post>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from "./components/Post.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components: {
+        Post
+    },
+    data(){
+        // some dummy data
+        return {
+            posts: [
+                {
+                    id: 'p1',
+                    title: 'post1',
+                    author: 'ben1',
+                    authorURL: 'ben1',
+                    votesCount: 999,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deserunt ipsam iure laboriosam expedita tempora totam, animi in dolorem vero. At, mollitia. Similique debitis aliquam, laudantium magni corporis et excepturi.',
+                },
+                {
+                    id: 'p2',
+                    title: 'post2',
+                    author: 'ben2',
+                    authorURL: 'ben2',
+                    votesCount: 99,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deserunt ipsam iure laboriosam expedita tempora totam, animi in dolorem vero. At, mollitia. Similique debitis aliquam, laudantium magni corporis et excepturi.',
+                },
+                {
+                    id: 'p3',
+                    title: 'post3',
+                    author: 'ben3',
+                    authorURL: 'ben3',
+                    votesCount: 9,
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deserunt ipsam iure laboriosam expedita tempora totam, animi in dolorem vero. At, mollitia. Similique debitis aliquam, laudantium magni corporis et excepturi.',
+                }
+            ]
+        }
+    }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    html, body{
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+    }
+
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 100%;
+
+        div.header{
+            height: 7%;
+        }
+
+        div.posts-container{
+            height: 75%;
+            overflow: scroll;
+            margin: 0 20px;
+            border: 1px solid #cccccc;
+            border-radius: 4px;
+        }
+
+    }
 </style>
---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,145 @@
+<template lang="html">
+    <div class="post-block" :data-id="post.id">
+        <div class="rank">
+            <span>{{ i + 1 }}</span>
+        </div>
+        <div class="vote-actions">
+            <div class="vote-buttons">
+                <button class="vote-up" @click="voteUp">
+                    <i class="far fa-thumbs-up"></i>
+                </button>
+                <div class="votes-count">
+                    {{ post.votesCount }}
+                </div>
+                <button class="vote-down" @click="voteDown">
+                    <i class="far fa-thumbs-down"></i>
+                </button>
+            </div>
+        </div>
+        <div class="post">
+            <div class="post-author">
+                <span>Posted by : {{ post.author }}</span>
+                <a :href="post.authorURL"></a>
+            </div>
+            <div class="post-title">
+                <span>
+                    <a href="">
+                        <h2>{{ post.title }}</h2>
+                    </a>
+                </span>
+            </div>
+            <div class="post-content">
+                <p>{{ post.content }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: {
+            type: Object,
+            default: () => {
+                return {
+                    id: '',
+                    title: '',
+                    author: '',
+                    authorURL: '',
+                    votesCount: 0,
+                    content: '',
+                }
+            }
+        },
+        i: Number
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    created() {
+
+    },
+
+    mounted() {
+        this.$nextTick( () => {
+
+        })
+    },
+
+    methods: {
+        voteUp(){
+            // do vote up request
+            // location.reload()
+        },
+
+        voteDown(){
+            // do vote down request
+            // location.reload()
+        },
+
+    },
+
+}
+</script>
+
+<style lang="scss">
+    .post-block{
+        position: relative;
+        margin: 40px 60px;
+        padding-left: 60px;
+        border: 1px solid #cccccc;
+        border-radius: 4px;
+
+        .rank{
+            position: absolute;
+            width: 20px;
+            height: 100%;
+            align-items: center;
+            display: flex;
+            left: 0;
+            background-color: #eeeeee;
+
+            span{
+                margin: auto;
+                &::before{
+                    content: "#";
+                }
+            }
+        }
+
+        .vote-actions{
+            position: absolute;
+            width: 40px;
+            height: 100%;
+            align-items: center;
+            display: flex;
+            left: 20px;
+            background-color: #f8f9fa;
+
+            .vote-buttons{
+                margin: auto;
+
+                button{
+                    border: none;
+                    cursor: pointer;
+                    background-color: transparent;
+                }
+            }
+        }
+
+        .post{
+            position: relative;
+            text-align: left;
+            padding-left: 8px;
+
+            a{
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+</style>
